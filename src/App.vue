2fe3f599b3509aef6851cfb3633b6322f1316fed
peter <ph@mastery.sk>
2025-06-23 22:28:45 +0200

    feat: odstrániť logo a pridať nadpis do komponentu App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Konvertor mien</h1>
 
     <div class="wrapper">
       <nav>
